1e992929ec83c091a57fbfe918636e608d086938
Sergey Krylov <drinkin.piano@gmail.com>
2015-10-29 10:45:56 +0300

    my_habr

---
+++ test/controllers/welcome_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class WelcomeControllerTest < ActionController::TestCase
+  test "should get title:string" do
+    get :title:string
+    assert_response :success
+  end
+
+  test "should get body:text" do
+    get :body:text
+    assert_response :success
+  end
+
+end
